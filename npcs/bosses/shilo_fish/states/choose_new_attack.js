b7649e0239be7175b6eebca8a9b158e1f7310b2b
xander-lambright <xanderlambright@gmail.com>
2024-08-25 15:27:14 -0700

    added all attacks back to big fish boss

--- npcs/bosses/shilo_fish/states/choose_new_attack.js
+++ npcs/bosses/shilo_fish/states/choose_new_attack.js
@@ -1,6 +1,6 @@
 var state_choose_new_attack = new State("choose_new_attack")
 
 state_choose_new_attack.enter = function (e) {
-    var possible_states = ["jump_cascade"]
+    var possible_states = ["jump_cascade", "rolling_state", "crush_jumping"]
     StateMachine.transitionToState("choose_new_attack", getRandomElement(possible_states), e)
 }
