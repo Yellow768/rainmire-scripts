a09b54044bee5b42618e700efcb7bafad8ec682a
xander-lambright <xanderlambright@gmail.com>
2024-09-18 01:00:40 -0700

    converted sea mine back to an npc.
    did this because canSeeEntity is very useful
    
    I dont want to wait for waterloggable scripted blocks.
    
    removed visibility changes as that was causing the weird issues

--- blocks/sea_mine.js
+++ blocks/sea_mine.js
@@ -1,104 +1,95 @@
-var block
-var range = 4
 var api = Java.type('noppes.npcs.api.NpcAPI').Instance();
 load(api.getLevelDir() + '/scripts/ecmascript/boiler/commonFunctions.js')
 function init(e) {
-    block = e.block
-    e.block.storeddata.put("sea_mine", 1)
-    e.block.setGeckoAnimationFile("iob:animations/sea_mine_animations.json")
-    if (!e.block.timers.has(4)) {
+    if (!e.npc.storeddata.has("y_value")) {
+        e.npc.storeddata.put("y_value", e.npc.y)
+    }
+    if (e.npc.isAlive()) {
         resetMine(e)
-        e.block.timers.forceStart(3, 0, true)
+        e.npc.timers.forceStart(3, 0, true)
     }
+    e.npc.storeddata.remove("exploded")
 }
-function explode() {
-    if (block.timers.has(4)) return
-    block.setGeckoTexture("iob:textures/skins/empty.png")
-    block.world.explode(block.x, block.y, block.z, range + 2, false, false)
-    block.world.spawnParticle("alexsmobs:skulk_boom", block.x, block.y + 1, block.z, 0, 0, 0, 0, 1)
-    block.world.spawnParticle("aquamirae:ghost_shine", block.x - .5, block.y + 1, block.z - .5, .5, .5, .5, 1, 30)
-    block.world.playSoundAt(block.pos, "alexsmobs:sculk_boomer", 1, .2)
-    block.world.playSoundAt(block.pos, "alexsmobs:void_portal_open", 1, .2)
-    block.timers.stop(1)
-    block.timers.stop(2)
-    block.timers.forceStart(4, 200, false)
-    block.setIsPassible(true)
-    block.setLight(0)
+function explode(e) {
+    resetMine(e)
+    e.npc.updateClient()
+    e.npc.world.explode(e.npc.x, e.npc.y, e.npc.z, e.npc.stats.getAggroRange() + 2, false, false)
+    e.npc.world.spawnParticle("alexsmobs:skulk_boom", e.npc.x, e.npc.y + 1, e.npc.z, 0, 0, 0, 0, 1)
+    e.npc.world.spawnParticle("aquamirae:ghost_shine", e.npc.x - .5, e.npc.y + 1, e.npc.z - .5, .5, .5, .5, 1, 30)
+    e.npc.world.playSoundAt(e.npc.pos, "alexsmobs:sculk_boomer", 1, .2)
+    e.npc.world.playSoundAt(e.npc.pos, "alexsmobs:void_portal_open", 1, .2)
+    e.npc.timers.stop(1)
+    e.npc.setHealth(0)
+    e.npc.timers.stop(2)
+    e.npc.executeCommand("/setblock ~ " + Math.round(e.npc.storeddata.get("y_value")) + " ~ water")
+}
 
-    for (var x = -range; x <= range; x++) {
-        for (var z = -range; z <= range; z++) {
-            for (var y = -range; y <= range; y++) {
-                var check_block = block.world.getBlock(block.x + x, block.y + y, block.z + z)
-                if (check_block.storeddata.has("sea_mine") && check_block != block) {
-                    check_block.trigger(1, [])
-                }
-            }
-        }
+function interact(e) {
+    if (e.player.gamemode == 1) {
+        e.npc.storeddata.put("y_value", e.npc.y)
+        e.npc.say("Y Value set")
     }
 }
 
-function trigger(e) {
-    if (e.id == 1) {
-        explode()
-    }
+
+function died(e) {
+    explode(e)
 }
 
 function collide(e) {
-    if (e.block.timers.has(4)) return
     if (e.entity.type == 1 && e.entity.gamemode == 1) return
-    explode()
+    if (e.entity.name == "Ancient Sea Mine") return
+    if (TrueDistanceCoord(e.npc.x, e.npc.y, e.npc.z, e.entity.x, e.entity.y, e.entity.z) > 1.4) return
+    e.npc.kill()
 }
 
 function timer(e) {
     if (e.id == 1) {
-        explode()
+        e.npc.kill()
     }
 
     if (e.id == 2) {
 
-        if (!detectIfEntitiesInRange(e, range + 2)) {
+        if (!detectIfEntitiesInRange(e, e.npc.stats.getAggroRange() + 1)) {
             resetMine(e)
-            e.block.world.playSoundAt(e.block.pos, "alexsmobs:void_portal_open", 1, 2)
-            e.block.timers.stop(2)
-            e.block.executeCommand("/stopsound @a[distance=..30] * alexsmobs:void_portal_close")
+            e.npc.world.playSoundAt(e.npc.pos, "alexsmobs:void_portal_open", 1, 2)
+            e.npc.timers.stop(2)
+            e.npc.executeCommand("/stopsound @a[distance=..30] * alexsmobs:void_portal_close")
         }
     }
     if (e.id == 3) {
-        if (!e.block.timers.has(1) && detectIfEntitiesInRange(e, range) && !e.block.timers.has(4)) {
-            e.block.timers.forceStart(1, 60, false)
-            e.block.timers.forceStart(2, 0, true)
+        if (!e.npc.timers.has(1) && detectIfEntitiesInRange(e, e.npc.stats.getAggroRange()) && e.npc.isAlive()) {
+            e.npc.timers.forceStart(1, 60, false)
+            e.npc.timers.forceStart(2, 0, true)
             var builder = e.API.createAnimBuilder()
             builder.thenLoop("animation.sea_mine.spin")
-            e.block.syncAnimForAll(builder)
-            e.block.world.playSoundAt(e.block.pos, "alexsmobs:void_portal_close", 1, .6)
-            e.block.world.playSoundAt(e.block.pos, "minecraft:block.beacon.deactivate", 1, .6)
-            e.block.setGeckoIdleAnimation("animation.sea_mine.spin")
-            e.block.setLight(13)
+            e.npc.syncAnimationsForAll(builder)
+            e.npc.world.playSoundAt(e.npc.pos, "alexsmobs:void_portal_close", 1, .6)
+            e.npc.world.playSoundAt(e.npc.pos, "minecraft:block.beacon.deactivate", 1, .6)
+            e.npc.executeCommand("/setblock ~ " + Math.round(e.npc.storeddata.get("y_value")) + " ~ minecraft:light[level=10,waterlogged=true]")
         }
-    }
-    if (e.id == 4) {
-        resetMine(e)
     }
 }
 function resetMine(e) {
-    e.block.setGeckoModel("iob:geo/sea_mine.geo.json")
-    e.block.setGeckoTexture("iob:textures/skins/sea_mine.png")
-    e.block.setGeckoIdleAnimation("animation.sea_mine.idle")
     var builder = e.API.createAnimBuilder()
     builder.thenLoop("animation.sea_mine.idle")
-    e.block.syncAnimForAll(builder)
-    e.block.timers.stop(1)
-    e.block.setIsPassible(false)
+    e.npc.syncAnimationsForAll(builder)
+    e.npc.timers.stop(1)
+    if (e.npc.world.getBlock(e.npc.x, e.npc.y, e.npc.z).name == "minecraft:water" || (e.npc.world.getBlock(e.npc.x, e.npc.y, e.npc.z).name == "minecraft:light" && e.npc.world.getBlock(e.npc.x, e.npc.y, e.npc.z).getProperty("level") != 5)) {
+        e.npc.executeCommand("/setblock ~ " + Math.round(e.npc.storeddata.get("y_value")) + " ~ minecraft:light[level=5,waterlogged=true]")
+    }
+    e.npc.y = e.npc.storeddata.get("y_value")
 
-    e.block.setLight(7)
 }
 function detectIfEntitiesInRange(e, range) {
-    var nE = e.block.world.getNearbyEntities(e.block.pos, range, 5)
-    if (nE.length > 0) {
+    var nE = e.npc.world.getNearbyEntities(e.npc.pos, range, 5)
+    if (nE.length > 1) {
         for (var i = 0; i < nE.length; i++) {
-            if (nE[i].type == 1 && nE[i].gamemode == 1) continue
-            if (nE[i].type == 2 && (nE[i].getFaction().getId() == 4 || nE[i].getFaction().getId() == 5)) continue
-            return true
+            if (nE[i] != e.npc && nE[i].name != "Ancient Sea Mine" && e.npc.canSeeEntity(nE[i])) {
+                if (nE[i].type == 1 && nE[i].gamemode == 1) continue
+                if (nE[i].type == 2 && !e.npc.getFaction().hostileToNpc(nE[i])) continue
+                return true
+            }
         }
     }
     return false
